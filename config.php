901d5b860727558a98fa60367c0bcf231372fac0
Douglas Morais <douglaserik.tec@gmail.com>
2020-05-08 10:00:15 -0400

    Gerando sessão

--- config.php
+++ config.php
@@ -1,4 +1,20 @@
 <?php
-    $retorno = array('informacao'=>"ilistraCode");
+    $link = 'https://ws.sandbox.pagseguro.uol.com.br/v2/session?
+    email=douglaserik.tec@gmail.com&
+    token=B75CECD11BD342D397AD8D2ED50C1419';
+    /*iniciando o cURL*/ 
+    $_curl = curl_init($link);
+    /*ESSE É O CONJUNTO DE CARACTERES. COMO UTILIZAMOS CARACTERISTICAS ESPECIAIS, ENTÃO COLOCAMOS POR PADRÃO UTF-8*/ 
+    curl_setup($_curl, CURLOPT_HTTPHEADER, array("Content-Type: application/x-www-form-urlencoded; charset=UTF-8"));
+    /*Autorizando o retorno de informação em string*/
+    curl_setup($_curl, CURLOPT_RETURNTRANSFER, true);
+    /*Tipo de transferencia. o PagSeguro exige que seja por POST, entçao colocamos TRUE*/
+    curl_setup($_curl, CURLOP_POST, true);
+
+    /**Executando o cURL */
+    $retorno = curl_exec($_curl);
+    curl_close($_curl);/**Encerrando o cURL*/
+
+    $retorno = simplexml_load_string($retorno);//Convertendo o XML em uma string
     echo json_encode($retorno);
 ?>
